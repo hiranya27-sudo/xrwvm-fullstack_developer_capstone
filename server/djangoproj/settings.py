88d9691e05b79338d688d0cdad0e70340d3b6ac4
hiranya27-sudo <hiranyapiumath@gmail.com>
2026-06-09 13:56:19 -0400

    2nd part is done

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','https://bushidohinno-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = [ 'https://bushidohinno-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.proxy.cognitiveclass.ai",
+    "https://*.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,8 +65,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +143,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+	os.path.join(BASE_DIR, 'frontend/build'),
+	os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
+
 
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,5 +1,8 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+// ... inside your <Routes> or <Switch>:
+<Route path="/register" element={<Register />} />
 
 function App() {
   return (
